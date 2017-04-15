b2d779c824b4f72cd68739b4c33618a5530c53ed
apple <maheshbabu.somineni@gmail.com>
2017-04-15 16:56:51 -0400

    added

---
+++ CalculatorCodeChallenge/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  CalculatorCodeChallenge
+//
+//  Created by apple on 15/04/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
